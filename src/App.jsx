6a58e67a1476288ea5e29f39b223b87399dfe2dc
Dinuja <dinujaubeysinghe0@gmail.com>
2026-05-21 07:37:31 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+    <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -115,6 +116,7 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+      
     </>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex items-center justify-between p-4 bg-gray-800 text-white'>
+      This is the NavBar
+    </div>
+  )
+}
+
+export default NavBar
